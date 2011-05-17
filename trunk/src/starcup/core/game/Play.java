725ce9de462c9556a446634c9b76468bb6f344d6
p.chenliang@gmail.com <p.chenliang@gmail.com@3f29d4e4-78b5-8fe9-0b4b-339e7e715787>
2011-05-17 09:39:49 +0000

    如何打出手牌的方法需要继续完善

--- trunk/src/starcup/core/game/Play.java
+++ trunk/src/starcup/core/game/Play.java
@@ -10,15 +10,15 @@ import starcup.core.component.Card;
  *
  * @author Administrator
  */
-public interface Action {
-
-    void drawCard(int number);
-
+public interface Play {
+    /**玩家摸一张牌*/
+    void drawCard(Card card);
+    /**玩家打出一张牌*/
     void playCard(Card card);
-
+    /**执行购买动作*/
     void buy();
-
+    /**执行合成动作*/
     void compose();
-
+    /*执行提炼动作*/
     void extract();
 }
--- trunk/src/starcup/ui/card/JCard.java
+++ trunk/src/starcup/ui/card/JCard.java
@@ -4,41 +4,53 @@
  */
 package starcup.ui.card;
 
-import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import starcup.ui.desk.PlayerDesk;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Akira.Pan
  */
-public class Card extends JLabel implements starcup.core.component.Card {
+public class JCard extends JLabel implements starcup.core.component.Card {
 
     private boolean seleceted = false;
-    private int width = 127;
-    private int height = 220;
+    /*默认宽度*/
+    public final static int CARD_WIDTH = 127;
+    /*整体长度*/
+    public final static int CARD_HEIGHT = 220;
+    /*图片路径*/
+    private String imageUrl = "src/starcup/resources/card/character.jpg";
+    private JPanel locatePanel;
 
-    public Card() {
-        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(starcup.StarCupApp.class).getContext().getResourceMap(PlayerDesk.class);
-        this.setIcon(resourceMap.getIcon("cardImage.icon")); // NOI18N
+    public JCard() {
+        this.setIcon(new ImageIcon(imageUrl));
         this.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        this.setCardSize(this.width, this.height);
+        this.setCardSize(JCard.CARD_WIDTH, JCard.CARD_HEIGHT);
         this.setName("cardImage"); // NOI18N
         this.setToolTipText("<html>卡片名称<br>基本描述</html>");
         this.addMouseListener(new java.awt.event.MouseAdapter() {
 
             @Override
             public void mouseClicked(java.awt.event.MouseEvent evt) {
+                onPlay();
+            }
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
                 onClicked();
             }
         });
-
     }
 
     private void setCardSize(int x, int y) {
         this.setMaximumSize(new java.awt.Dimension(x, y));
         this.setMinimumSize(new java.awt.Dimension(x, y));
         this.setPreferredSize(new java.awt.Dimension(x, y));
+    }
+
+    public void onPlay() {
+        this.locatePanel.add(this);
     }
 
     public void onClicked() {
@@ -51,5 +63,13 @@ public class Card extends JLabel implements starcup.core.component.Card {
 
         }
 
+    }
+
+    public JPanel getLocatePanel() {
+        return locatePanel;
+    }
+
+    public void setLocatePanel(JPanel locatePanel) {
+        this.locatePanel = locatePanel;
     }
 }
