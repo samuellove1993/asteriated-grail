75db4bc9751aeb974de6ad88f1835ce1a63db4db
p.chenliang@gmail.com <p.chenliang@gmail.com@3f29d4e4-78b5-8fe9-0b4b-339e7e715787>
2011-05-17 08:04:30 +0000

    修改手牌区的手牌排列方式

--- trunk/src/starcup/ui/desk/PlayerDesk.java
+++ trunk/src/starcup/ui/desk/PlayerDesk.java
@@ -29,7 +29,8 @@ public class PlayerDesk extends javax.swing.JPanel {
     public void display() {
         for (int i = 0; i < player.getCardLimit(); i++) {
             Card card = new Card();
-            handDeckPanel.add(card);
+            handDeckPanel.add(card, new org.netbeans.lib.awtextra.AbsoluteConstraints(i*60, 50 , -1, -1));
+       //     handDeckPanel.add(card);
             this.revalidate();
         }
     }
@@ -51,13 +52,17 @@ public class PlayerDesk extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jFormattedTextField1 = new javax.swing.JFormattedTextField();
         cardImage = new javax.swing.JLabel();
         publicPanel = new javax.swing.JPanel();
         playerPanel = new javax.swing.JPanel();
         handDeckPanel = new javax.swing.JPanel();
 
-        cardImage.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(starcup.StarCupApp.class).getContext().getResourceMap(PlayerDesk.class);
+        jFormattedTextField1.setText(resourceMap.getString("jFormattedTextField1.text")); // NOI18N
+        jFormattedTextField1.setName("jFormattedTextField1"); // NOI18N
+
+        cardImage.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         cardImage.setIcon(resourceMap.getIcon("cardImage.icon")); // NOI18N
         cardImage.setText(resourceMap.getString("cardImage.text")); // NOI18N
         cardImage.setToolTipText(resourceMap.getString("cardImage.toolTipText")); // NOI18N
@@ -97,22 +102,22 @@ public class PlayerDesk extends javax.swing.JPanel {
         handDeckPanel.setMinimumSize(new java.awt.Dimension(600, 300));
         handDeckPanel.setName("handDeckPanel"); // NOI18N
         handDeckPanel.setPreferredSize(new java.awt.Dimension(600, 300));
+        handDeckPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
         playerPanel.add(handDeckPanel);
 
         add(playerPanel);
     }// </editor-fold>//GEN-END:initComponents
 
     private void cardImageMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cardImageMouseClicked
-    
     }//GEN-LAST:event_cardImageMouseClicked
 
     private void cardImageMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cardImageMouseEntered
         // TODO add your handling code here:
     }//GEN-LAST:event_cardImageMouseEntered
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel cardImage;
     private javax.swing.JPanel handDeckPanel;
+    private javax.swing.JFormattedTextField jFormattedTextField1;
     private javax.swing.JPanel playerPanel;
     private javax.swing.JPanel publicPanel;
     // End of variables declaration//GEN-END:variables
