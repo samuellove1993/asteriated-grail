df1b9d67475b4caeab1569f3f04ca94d0fc6b74b
p.chenliang@gmail.com <p.chenliang@gmail.com@3f29d4e4-78b5-8fe9-0b4b-339e7e715787>
2011-05-23 09:09:39 +0000

    拆分项目，将源代码分为两部分一部分为JAVA GUI的，一部分为游戏核心引擎的。下一步的目标是编写核心模拟游戏。

---
+++ trunk/core/starcup/core/game/IGame.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package starcup.core.game;
+
+/**
+ *
+ * @author Akira.Pan
+ */
+public interface IGame {
+    /**
+     * 获取当前游戏实例
+     * @return 
+     */
+    public IGame getGame();
+}
